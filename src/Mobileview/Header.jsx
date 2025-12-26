28749dde8fa2739e2552b5082553244242417c69
AMIREDDYRAKEY <rakeyr213@gmail.com>
2025-12-27 01:04:21 +0530

    first commit

--- src/Mobileview/Header.jsx
+++ src/Mobileview/Header.jsx
@@ -9,42 +9,60 @@ const Header = () => {
     { name: "MX Player", img: img1 },
     { name: "Pharmacy", img: img1 },
     { name: "Deals", img: img1 },
+    { name: "Bazaar", img: img1 },
+    { name: "MX Player", img: img1 },
+    { name: "Pharmacy", img: img1 },
+    { name: "Deals", img: img1 },
   ];
 
   return (
     <div className="bg-gradient-to-b from-red-600 to-red-400 p-5">
 
-      {/* CONTAINER */}
       <div className="max-w-5xl mx-auto">
 
-        {/* MOBILE — horizontal scroll */}
+
         <div className="flex gap-3 overflow-x-auto scrollbar-hide pb-1 snap-x snap-mandatory md:hidden">
           {shortcuts.map((item, i) => (
             <div
               key={i}
-              className="snap-start shrink-0 w-[78px] bg-white rounded-xl px-3 py-2
-              flex flex-col items-center text-[11px] shadow-sm"
+              className="snap-start shrink-0 w-[78px] bg-white rounded-xl
+              px-3 py-2 flex flex-col items-center text-[11px] shadow-sm"
             >
-              <img src={item.img} alt={item.name} className="h-9 w-9 mb-1 object-contain" />
+              <img
+                src={item.img}
+                alt={item.name}
+                className="h-9 w-9 mb-1 object-contain"
+              />
               <p className="text-center leading-tight">{item.name}</p>
             </div>
           ))}
         </div>
-
-        {/* TABLET / DESKTOP — grid */}
-        <div className="hidden md:grid grid-cols-3 lg:grid-cols-6 gap-4">
+        {/* tablet */}
+        <div className="hidden md:grid 
+  grid-flow-col auto-cols-max 
+  gap-2 overflow-x-auto scrollbar-hide pb-1 
+  snap-x snap-mandatory"
+        >
           {shortcuts.map((item, i) => (
             <div
               key={i}
-              className="bg-white rounded-xl py-3 px-4 shadow-sm
-              flex flex-col items-center text-sm
-              hover:shadow-md transition"
+              className="snap-start bg-white rounded-xl
+      md:h-[70px] md:w-[100px]
+      lg:px-2 lg:py-3
+      shadow-sm flex flex-col items-center justify-center
+      md:text-xs lg:text-sm
+      hover:shadow-md transition"
             >
-              <img src={item.img} alt={item.name} className="h-10 w-10 mb-1 object-contain" />
+              <img
+                src={item.img}
+                alt={item.name}
+                className="md:h-7 md:w-7 lg:h-10 lg:w-10 mb-1 object-contain"
+              />
               <p className="text-center font-medium">{item.name}</p>
             </div>
           ))}
         </div>
+
 
       </div>
     </div>
